1e68c0ff3d8d0f4ecc316b310d0a38b8260a8054
Andrew Hazelden <andrew@andrewhazelden.com>
2015-12-03 17:47:34 -0400

    getRenderParameter

--- mxs2ascii/mxs2ascii.py
+++ mxs2ascii/mxs2ascii.py
@@ -1,6 +1,6 @@
 # Maxwell MXS to ASCII Translator
 # --------------------------------------------
-# 2015-12-03 5.34 pm v0.1
+# 2015-12-03 5.47 pm v0.1
 # By Andrew Hazelden 
 # Email: andrew@andrewhazelden.com
 # Blog: http://www.andrewhazelden.com
@@ -96,14 +96,14 @@ def b2a_writeAsciiScene(mxsFilePath):
   active_camera = cameraName
 
   # Scene
-  time_limit = 1440
-  sampling_level = 16
-  multilight = 'intensity'
-  multilight_output = 'composite'
-  cpu_threads = 'Automatic'
-  priority = 'low'
-  quality = 'production'
-  command_line = ''
+  time_limit = scene.getRenderParameter('STOP TIME')[0] / 60
+  sampling_level = scene.getRenderParameter('SAMPLING LEVEL')[0]
+  # multilight = 'intensity'
+  # multilight_output = 'composite'
+  cpu_threads = scene.getRenderParameter('NUM THREADS')[0]
+  # priority = 'low'
+  # quality = 'production'
+  # command_line = ''
 
   # Output
   depth = 16
@@ -119,75 +119,77 @@ def b2a_writeAsciiScene(mxsFilePath):
   materials_search_path = '/Users/Andrew/Documents/maxwell'
 
   # Globals
-  motion_blur = 'on'
-  displacement = 'on'
-  dispersion = 'on'
+  # motion_blur = 'on'
+  # displacement = 'on'
+  # dispersion = 'on'
 
   # Extra Sampling
-  extra_sampling_enabled = 'off'
-  extra_sampling_mask = 'Custom'
-  extra_sampling_level = 14
-  extra_sampling_custom_alpha = ''
-  extra_sampling_bitmap = ''
-  extra_sampling_invert_mask = 'off'
+  # extra_sampling_enabled = 'off'
+  # extra_sampling_mask = 'Custom'
+  # extra_sampling_level = 14
+  # extra_sampling_custom_alpha = ''
+  # extra_sampling_bitmap = ''
+  # extra_sampling_invert_mask = 'off'
   
   # Channels
-  channels_output_mode = 'embedded'
-  channels_render = 'off'
-  channels_alpha = 'off'
-  channels_opaque = 'off'
-  channels_zbuffer = 'off'
-  channels_zbuffer_meters = "1.0 1.0"
-  channels_shadow = 'off'
-  channels_material_id = 'off'
-  channels_object_id = 'off'
-  channels_motion_vector = 'off'
-  channels_roughness = 'off'
-  channels_fresnel = 'off'
-  channels_normals = 'off'
-  channels_normals_mode = 'World'
-  channels_position  = 'off'
-  channels_position_mode = 'World'
-  channels_deep = 'off'
-  channels_deep_mode = 'Alpha'
-  channels_deep_min_distance_meters = 0.2
-  channels_deep_max_samples = 20
-  channels_uv = 'off'
-  channels_custom_alpha = 'off'
-  channels_reflectance = 'off'
+  # channels_output_mode = 'embedded'
+  # channels_render = 'off'
+  # channels_alpha = 'off'
+  # channels_opaque = 'off'
+  # channels_zbuffer = 'off'
+  # channels_zbuffer_meters = "1.0 1.0"
+  # channels_shadow = 'off'
+  # channels_material_id = 'off'
+  # channels_object_id = 'off'
+  # channels_motion_vector = 'off'
+  # channels_roughness = 'off'
+  # channels_fresnel = 'off'
+  # channels_normals = 'off'
+  # channels_normals_mode = 'World'
+  # channels_position  = 'off'
+  # channels_position_mode = 'World'
+  # channels_deep = 'off'
+  # channels_deep_mode = 'Alpha'
+  # channels_deep_min_distance_meters = 0.2
+  # channels_deep_max_samples = 20
+  # channels_uv = 'off'
+  # channels_custom_alpha = 'off'
+  # channels_reflectance = 'off'
 
   # Tone Mapping
   tone_mapping_color_space = b2a_getColorSpace(scene)
-  tone_mapping_white_point = 6500
-  tone_mapping_tint = 0.0
+  tone_mapping_white_point, tone_mapping_tint, ok = scene.getWhitePoint()
+  # tone_mapping_white_point = 6500
+  # tone_mapping_tint = 0.0
   tone_mapping_monitor_gamma,tone_mapping_burn,ok = scene.getToneMapping()
-  #tone_mapping_monitor_gamma =  scene.getColorSpaceGamma()
-  tone_mapping_sharpness_enabled = 'off'
-  tone_mapping_sharpness = 60.0
+  # tone_mapping_monitor_gamma =  scene.getColorSpaceGamma()
+  # tone_mapping_sharpness_enabled = 'off'
+  # tone_mapping_sharpness = 60.0
 
   # Simulens
+    
   #isEnabled,simulens_diffraction,simulens_frequency,simulens_aperture_map,simulens_obstacle_map,ok = scene.getDiffraction()
-  simulens_aperture_map = ''
-  simulens_obstacle_map = ''
-  simulens_diffraction = 1250.0
-  simulens_frequency = 1250.0
-  simulens_scattering = 250.0
-  simulens_devingetting = 100.0
+  # simulens_aperture_map = ''
+  # simulens_obstacle_map = ''
+  # simulens_diffraction = 1250.0
+  # simulens_frequency = 1250.0
+  # simulens_scattering = 250.0
+  # simulens_devingetting = 100.0
 
   # Illumination and Caustics
-  illumination = 'Both'
-  reflection_caustics = 'Both'
-  refraction_caustics = 'Both'
+  # illumination = 'Both'
+  # reflection_caustics = 'Both'
+  # refraction_caustics = 'Both'
 
   # Fire
-  fire_floating_shadows = 'off'
-  fire_floating_refractions = 'off'
+  # fire_floating_shadows = 'off'
+  # fire_floating_refractions = 'off'
 
   # Overlay Text
-  overlay_text = ''
-  overlay_text_position = ''
-  overlay_text_color = '1.0 1.0 1.0'
-  overlay_text_background = '0.0 0.0 0.0'
+  # overlay_text = ''
+  # overlay_text_position = ''
+  # overlay_text_color = '1.0 1.0 1.0'
+  # overlay_text_background = '0.0 0.0 0.0'
   
 
   # Check the OS time
@@ -217,12 +219,12 @@ def b2a_writeAsciiScene(mxsFilePath):
   textDocument += indent + 'active_camera "' + str(active_camera) + '"\n'
   textDocument += indent + 'time_limit ' + str(time_limit) + '\n'
   textDocument += indent + 'sampling_level ' + str(sampling_level) + '\n'
-  textDocument += indent + 'multilight "' + str(multilight) + '"\n'
-  textDocument += indent + 'multilight_output "' + str(multilight_output) + '"\n'
-  textDocument += indent + 'cpu_threads "' + str(cpu_threads) + '"\n'
-  textDocument += indent + 'priority "' + str(priority) + '"\n'
-  textDocument += indent + 'quality "' + str(quality) + '"\n'
-  textDocument += indent + 'command_line "' + str(command_line) + '"\n'
+  # textDocument += indent + 'multilight "' + str(multilight) + '"\n'
+  # textDocument += indent + 'multilight_output "' + str(multilight_output) + '"\n'
+  textDocument += indent + 'cpu_threads ' + str(cpu_threads) + '\n'
+  # textDocument += indent + 'priority "' + str(priority) + '"\n'
+  # textDocument += indent + 'quality "' + str(quality) + '"\n'
+  # textDocument += indent + 'command_line "' + str(command_line) + '"\n'
 
   textDocument += indent + 'depth ' + str(depth) + '\n'
   textDocument += indent + 'image "' + str(image) + '"\n'
@@ -233,68 +235,68 @@ def b2a_writeAsciiScene(mxsFilePath):
   textDocument += indent + 'materials_default "' + str(materials_default) + '"\n'
   textDocument += indent + 'materials_search_path "' + str(materials_search_path) + '"\n'
 
-  textDocument += indent + 'motion_blur ' + str(motion_blur) + '\n'
-  textDocument += indent + 'displacement ' + str(displacement) + '\n'
-  textDocument += indent + 'dispersion ' + str(dispersion) + '\n'
+  # textDocument += indent + 'motion_blur ' + str(motion_blur) + '\n'
+  # textDocument += indent + 'displacement ' + str(displacement) + '\n'
+  # textDocument += indent + 'dispersion ' + str(dispersion) + '\n'
 
-  textDocument += indent + 'extra_sampling_enabled ' + str(extra_sampling_enabled) + '\n'
-  textDocument += indent + 'extra_sampling_mask "' + str(extra_sampling_mask) + '"\n'
-  textDocument += indent + 'extra_sampling_level ' + str(extra_sampling_level) + '\n'
-  textDocument += indent + 'extra_sampling_custom_alpha ' + str(extra_sampling_custom_alpha) + '\n'
-  textDocument += indent + 'extra_sampling_bitmap ' + str(extra_sampling_bitmap) + '\n'
-  textDocument += indent + 'extra_sampling_invert_mask ' + str(extra_sampling_invert_mask) + '\n'
+  # textDocument += indent + 'extra_sampling_enabled ' + str(extra_sampling_enabled) + '\n'
+  # textDocument += indent + 'extra_sampling_mask "' + str(extra_sampling_mask) + '"\n'
+  # textDocument += indent + 'extra_sampling_level ' + str(extra_sampling_level) + '\n'
+  # textDocument += indent + 'extra_sampling_custom_alpha ' + str(extra_sampling_custom_alpha) + '\n'
+  # textDocument += indent + 'extra_sampling_bitmap ' + str(extra_sampling_bitmap) + '\n'
+  # textDocument += indent + 'extra_sampling_invert_mask ' + str(extra_sampling_invert_mask) + '\n'
 
-  textDocument += indent + 'channels_output_mode "' + str(channels_output_mode) + '"\n'
-  textDocument += indent + 'channels_render ' + str(channels_render) + '\n'
-  textDocument += indent + 'channels_alpha ' + str(channels_alpha) + '\n'
-  textDocument += indent + 'channels_opaque ' + str(channels_opaque) + '\n'
-  textDocument += indent + 'channels_zbuffer ' + str(channels_zbuffer) + '\n'
-  textDocument += indent + 'channels_zbuffer_meters ' + str(channels_zbuffer_meters) + '\n'
-  textDocument += indent + 'channels_shadow ' + str(channels_shadow) + '\n'
-  textDocument += indent + 'channels_material_id ' + str(channels_material_id) + '\n'
-  textDocument += indent + 'channels_object_id ' + str(channels_object_id) + '\n'
-  textDocument += indent + 'channels_motion_vector ' + str(channels_motion_vector) + '\n'
-  textDocument += indent + 'channels_roughness ' + str(channels_roughness) + '\n'
-  textDocument += indent + 'channels_fresnel ' + str(channels_fresnel) + '\n'
-  textDocument += indent + 'channels_normals ' + str(channels_normals) + '\n'
-  textDocument += indent + 'channels_normals_mode "' + str(channels_normals_mode) + '"\n'
-  textDocument += indent + 'channels_position ' + str(channels_position) + '\n'
-  textDocument += indent + 'channels_position_mode "' + str(channels_position_mode) + '"\n'
-  textDocument += indent + 'channels_deep ' + str(channels_deep) + '\n'
-  textDocument += indent + 'channels_deep_mode "' + str(channels_deep_mode) + '"\n'
-  textDocument += indent + 'channels_deep_min_distance_meters ' + str(channels_deep_min_distance_meters) + '\n'
-  textDocument += indent + 'channels_deep_max_samples ' + str(channels_deep_max_samples) + '\n'
-  textDocument += indent + 'channels_uv ' + str(channels_uv) + '\n'
-  textDocument += indent + 'channels_custom_alpha ' + str(channels_custom_alpha) + '\n'
-  textDocument += indent + 'channels_reflectance ' + str(channels_reflectance) + '\n'
+  # textDocument += indent + 'channels_output_mode "' + str(channels_output_mode) + '"\n'
+  # textDocument += indent + 'channels_render ' + str(channels_render) + '\n'
+  # textDocument += indent + 'channels_alpha ' + str(channels_alpha) + '\n'
+  # textDocument += indent + 'channels_opaque ' + str(channels_opaque) + '\n'
+  # textDocument += indent + 'channels_zbuffer ' + str(channels_zbuffer) + '\n'
+  # textDocument += indent + 'channels_zbuffer_meters ' + str(channels_zbuffer_meters) + '\n'
+  # textDocument += indent + 'channels_shadow ' + str(channels_shadow) + '\n'
+  # textDocument += indent + 'channels_material_id ' + str(channels_material_id) + '\n'
+  # textDocument += indent + 'channels_object_id ' + str(channels_object_id) + '\n'
+  # textDocument += indent + 'channels_motion_vector ' + str(channels_motion_vector) + '\n'
+  # textDocument += indent + 'channels_roughness ' + str(channels_roughness) + '\n'
+  # textDocument += indent + 'channels_fresnel ' + str(channels_fresnel) + '\n'
+  # textDocument += indent + 'channels_normals ' + str(channels_normals) + '\n'
+  # textDocument += indent + 'channels_normals_mode "' + str(channels_normals_mode) + '"\n'
+  # textDocument += indent + 'channels_position ' + str(channels_position) + '\n'
+  # textDocument += indent + 'channels_position_mode "' + str(channels_position_mode) + '"\n'
+  # textDocument += indent + 'channels_deep ' + str(channels_deep) + '\n'
+  # textDocument += indent + 'channels_deep_mode "' + str(channels_deep_mode) + '"\n'
+  # textDocument += indent + 'channels_deep_min_distance_meters ' + str(channels_deep_min_distance_meters) + '\n'
+  # textDocument += indent + 'channels_deep_max_samples ' + str(channels_deep_max_samples) + '\n'
+  # textDocument += indent + 'channels_uv ' + str(channels_uv) + '\n'
+  # textDocument += indent + 'channels_custom_alpha ' + str(channels_custom_alpha) + '\n'
+  # textDocument += indent + 'channels_reflectance ' + str(channels_reflectance) + '\n'
 
   textDocument += indent + 'tone_mapping_color_space "' + str(tone_mapping_color_space) + '"\n'
   textDocument += indent + 'tone_mapping_white_point ' + str(tone_mapping_white_point) + '\n'
   textDocument += indent + 'tone_mapping_tint ' + str(tone_mapping_tint) + '\n'
   textDocument += indent + 'tone_mapping_burn ' + str(tone_mapping_burn) + '\n'
   textDocument += indent + 'tone_mapping_monitor_gamma ' + str(tone_mapping_monitor_gamma) + '\n'
-  textDocument += indent + 'tone_mapping_sharpness_enabled ' + str(tone_mapping_sharpness_enabled) + '\n'
-  textDocument += indent + 'tone_mapping_sharpness ' + str(tone_mapping_sharpness) + '\n'
+  # textDocument += indent + 'tone_mapping_sharpness_enabled ' + str(tone_mapping_sharpness_enabled) + '\n'
+  # textDocument += indent + 'tone_mapping_sharpness ' + str(tone_mapping_sharpness) + '\n'
 
-  textDocument += indent + 'simulens_aperture_map ' + str(simulens_aperture_map) + '\n'
-  textDocument += indent + 'simulens_obstacle_map ' + str(simulens_obstacle_map) + '\n'
-  textDocument += indent + 'simulens_diffraction ' + str(simulens_diffraction) + '\n'
-  textDocument += indent + 'simulens_frequency ' + str(simulens_frequency) + '\n'
-  textDocument += indent + 'simulens_scattering ' + str(simulens_scattering) + '\n'
-  textDocument += indent + 'simulens_devingetting ' + str(simulens_devingetting) + '\n'
-  textDocument += indent + 'simulens_devingetting ' + str(simulens_devingetting) + '\n'
+  # textDocument += indent + 'simulens_aperture_map ' + str(simulens_aperture_map) + '\n'
+  # textDocument += indent + 'simulens_obstacle_map ' + str(simulens_obstacle_map) + '\n'
+  # textDocument += indent + 'simulens_diffraction ' + str(simulens_diffraction) + '\n'
+  # textDocument += indent + 'simulens_frequency ' + str(simulens_frequency) + '\n'
+  # textDocument += indent + 'simulens_scattering ' + str(simulens_scattering) + '\n'
+  # textDocument += indent + 'simulens_devingetting ' + str(simulens_devingetting) + '\n'
+  # textDocument += indent + 'simulens_devingetting ' + str(simulens_devingetting) + '\n'
 
-  textDocument += indent + 'illumination "' + str(illumination) + '"\n'
-  textDocument += indent + 'reflection_caustics "' + str(reflection_caustics) + '"\n'
-  textDocument += indent + 'refraction_caustics "' + str(refraction_caustics) + '"\n'
+  # textDocument += indent + 'illumination "' + str(illumination) + '"\n'
+  # textDocument += indent + 'reflection_caustics "' + str(reflection_caustics) + '"\n'
+  # textDocument += indent + 'refraction_caustics "' + str(refraction_caustics) + '"\n'
 
-  textDocument += indent + 'fire_floating_shadows ' + str(fire_floating_shadows) + '\n'
-  textDocument += indent + 'fire_floating_refractions ' + str(fire_floating_refractions) + '\n'
+  # textDocument += indent + 'fire_floating_shadows ' + str(fire_floating_shadows) + '\n'
+  # textDocument += indent + 'fire_floating_refractions ' + str(fire_floating_refractions) + '\n'
 
-  textDocument += indent + 'overlay_text ' + str(overlay_text) + '\n'
-  textDocument += indent + 'overlay_text_position ' + str(overlay_text_position) + '\n'
-  textDocument += indent + 'overlay_text_color ' + str(overlay_text_color) + '\n'
-  textDocument += indent + 'overlay_text_background ' + str(overlay_text_background) + '\n'
+  # textDocument += indent + 'overlay_text ' + str(overlay_text) + '\n'
+  # textDocument += indent + 'overlay_text_position ' + str(overlay_text_position) + '\n'
+  # textDocument += indent + 'overlay_text_color ' + str(overlay_text_color) + '\n'
+  # textDocument += indent + 'overlay_text_background ' + str(overlay_text_background) + '\n'
   
   # Close the render_options section
   textDocument += '}\n'
